99e067b054ce2b0030368358976d9cbd0f6f8f3d
Mahak491 <shreemahak01@gmail.com>
2024-09-08 22:31:54 +0530

    suggestion-system frontend and backend code

--- backend/index.js
+++ backend/index.js
@@ -40,7 +40,6 @@ app.post('/suggestions/:id/vote', (req, res) => {
   res.json(suggestion); 
 });
 
-// Start the server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
